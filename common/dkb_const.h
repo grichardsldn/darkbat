3abca155722e7f7dda10a84606031ebf8a19164c
graham <graham@06524c92-433b-46d9-8b76-4b464eb20642>
2009-01-02 15:49:06 +0000

    iInitial version.
    git-svn-id: file:///home/pi/svn/Darkbat@12 06524c92-433b-46d9-8b76-4b464eb20642

---
+++ common/dkb_const.h
@@ -0,0 +1,11 @@
+#define DKB_1_MAGIC (26)
+
+#define DKB_REF (0)
+#define DKB_CONNECTION_REF (4)
+#define DKB_POS (1)
+#define DKB_ANGLE (2)
+#define DKB_END (3)
+
+#define DKB_ELEMENT_LINE (64)
+#define DKB_ELEMENT_POINT (65)
+
