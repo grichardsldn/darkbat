b5eca9e343f870ddd5c681f3b00570ec586cc512
graham <graham@06524c92-433b-46d9-8b76-4b464eb20642>
2009-03-21 18:30:49 +0000

    Made DKB_POS by DKB_RELPOS, and DKB_POS is now the object position.
    git-svn-id: file:///home/pi/svn/Darkbat@31 06524c92-433b-46d9-8b76-4b464eb20642

--- common/dkb_const.h
+++ common/dkb_const.h
@@ -2,9 +2,10 @@
 
 #define DKB_REF (0)
 #define DKB_CONNECTION_REF (4)
-#define DKB_POS (1)
+#define DKB_RELPOS (1)
 #define DKB_ANGLE (2)
 #define DKB_END (3)
+#define DKB_POS (4)
 
 #define DKB_ELEMENT_LINE (64)
 #define DKB_ELEMENT_POINT (65)
